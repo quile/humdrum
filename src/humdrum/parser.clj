6e48f7ea26a49e4d0235ce998ff7a56040e8ea78
Kyle Dawkins <kdawkins@fastmail.fm>
2013-11-01 00:09:08 -0700

    Tweaks and fiddling with the parsed data

--- src/humdrum/parser.clj
+++ src/humdrum/parser.clj
@@ -1,20 +1,24 @@
+;;;-----------------------------------------------------
+;;; humdrum parser
+;;; This should be able to parse any arbitrary humdrum
+;;; file into a parse tree that can be rapidly traversed
+;;; by the toolkit.
+;;;-----------------------------------------------------
 (ns humdrum.parser
-  (:require [instaparse.core :as insta]))
-
+  (:require [instaparse.core :as insta]
+            [clojure.zip :as zip] :reload))
 
 (def file-parser
   (insta/parser "resources/file.grammar"))
 
+(defn remove-comments
+  "Removes global comments from raw parsed output"
+  ([tree] (remove-comments tree {}))
+  ([tree opts]
+    (remove #(-> % second first (= :global-comment-line)) tree)))
 
-(file-parser
-"!! bastard comment
-**foo\t**bar
-*thing\t*other
-!bing\t!bang
-pop\t.
-*-\t*-")
-
+;; just some test fun for now
 (def wtc1f02 (slurp "resources/scores/wtc1f02.krn"))
-
 (def parsed (file-parser wtc1f02))
 
+(remove-comments parsed)
